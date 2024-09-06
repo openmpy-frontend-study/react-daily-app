0a25b562c775843e4c4d2b41318ed90ec741b816
openmpy <suhwan@kakao.com>
2024-09-06 13:55:48 +0900

    페이지 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import Diary from "./pages/Diary";
 import Home from "./pages/Home";
@@ -6,13 +6,27 @@ import New from "./pages/New";
 import NotFound from "./pages/NotFound";
 
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/new">New</Link>
+        <Link to="/diary">Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
